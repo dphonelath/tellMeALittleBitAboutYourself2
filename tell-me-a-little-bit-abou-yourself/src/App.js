bf6b11f57845f40c7cf05fb555f7cf01baff2c9a
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-25 16:25:10 -0600

    added images

--- tell-me-a-little-bit-abou-yourself/src/App.js
+++ tell-me-a-little-bit-abou-yourself/src/App.js
@@ -6,7 +6,18 @@ import { useGesture } from 'react-use-gesture';
 import './App.css';
 
 const pages = [
-  
+  './jpg/TMLBAY-cover.jpg',
+  './jpg/deep-breaths.jpg',
+  './jpg/interview.jpg',
+  './jpg/tell-me-a-little-bit-about-yourself.jpg',
+  './jpg/see-me-or-CV2.jpg',
+  './jpg/analogue-soul.jpg',
+  './jpg/weaknesses.jpg',
+  './jpg/lunch-break-interlude.jpg',
+  './jpg/music-to-my-career.jpg',
+  './jpg/5-year-plan.jpg',
+  './jpg/salary-negotiation.jpg',
+  './jpg/back-cover2.jpg'
 ]
 
 function App() {
