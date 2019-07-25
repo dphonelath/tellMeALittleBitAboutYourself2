66d80c3b95de40471284f9a96972a534b2095c11
Daniel Phonelath <daniel.phonelath@gmail.com>
2019-07-25 16:12:35 -0600

    added images

--- tell-me-a-little-bit-abou-yourself/src/App.js
+++ tell-me-a-little-bit-abou-yourself/src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
+import clamp from 'lodash-es/clamp';
+import { useSprings, animated } from 'react-spring';
+import { useGesture } from 'react-use-gesture';
 import './App.css';
+
+const pages = [
+  
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
